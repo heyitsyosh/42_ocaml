93a4d63323362e4bc6203307c54f1bbd5a769a08
myoshika <47554536+heyitsyosh@users.noreply.github.com>
2025-12-20 19:04:11 +0100

    [feat]: Implement ex06, check predicate on all characters in string

---
+++ module00/ex06/ft_string_all.ml
@@ -0,0 +1,10 @@
+let ft_string_all func str =
+  let rec loop i =
+    if i < 0 then
+      true
+    else if not (func String.get str i) then
+      false
+    else
+      loop (i - 1)
+  in
+  loop (String.length str - 1)
