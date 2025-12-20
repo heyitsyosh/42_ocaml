d8c0f6a6b4a51493048e05aceea38c9a67e38c2f
maria <winterzaq1@gmail.com>
2025-12-20 19:06:39 +0100

    [fix]: Call predicate correctly

--- module00/ex06/ft_string_all.ml
+++ module00/ex06/ft_string_all.ml
@@ -2,7 +2,7 @@ let ft_string_all func str =
   let rec loop i =
     if i < 0 then
       true
-    else if not (func String.get str i) then
+    else if not (func (String.get str i)) then
       false
     else
       loop (i - 1)
