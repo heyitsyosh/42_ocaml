03ea48467d8ea0f9aa3354701ca91e02c12148df
maria <you@example.com>
2026-02-26 15:54:27 +0100

    [refactor]: Use test helper function

--- module00/tests/ex00_test.ml
+++ module00/tests/ex00_test.ml
@@ -1,10 +1,11 @@
 open Ft_test_sign
 
+let test n =
+    Printf.printf "%d is " n;
+    ft_test_sign n
+
 let () =
   print_endline "\x1b[4;37mft_test_sign:\x1b[0m";
-  print_string "42 is ";
-  ft_test_sign 42;
-  print_string "0 is ";
-  ft_test_sign 0;
-  print_string "-42 is ";
-  ft_test_sign (-42)
+  test 42;
+  test 0;
+  test (-42)
--- module00/tests/ex02_test.ml
+++ module00/tests/ex02_test.ml
@@ -1,13 +1,10 @@
 open Ft_power
 
+let test x y =
+  Printf.printf "%d ^ %d = %d\n" x y (ft_power x y)
+
 let () =
   print_endline "\x1b[4;37mft_power:\x1b[0m";
-  print_string "2 ^ 4 = ";
-  print_int(ft_power 2 4);
-  print_newline ();
-  print_string "3 ^ 0 = ";
-  print_int(ft_power 3 0);
-  print_newline ();
-  print_string "0 ^ 5 = ";
-  print_int(ft_power 0 5);
-  print_endline "";
+  test 2 4;
+  test 3 0;
+  test 0 5
