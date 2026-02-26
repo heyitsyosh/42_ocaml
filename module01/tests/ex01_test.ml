eaa206e13b0731fc6afe48c5cdeb4250ab7f9759
maria <you@example.com>
2026-02-26 17:47:31 +0100

    [test]: Add tests for ex00 and ex01

---
+++ module01/tests/ex01_test.ml
@@ -0,0 +1,22 @@
+open Repeat_string
+
+let test n =
+  Printf.printf "%3d → \"%s\"\n" n (repeat_string n)
+
+let test_with_str str n =
+  Printf.printf "%3d * \"%s\" → \"%s\"\n" n str (repeat_string ~str n)
+
+let () =
+  print_endline "\x1b[4;37mrepeat_string tests:\x1b[0m";
+  print_endline "[Default string]";
+  test (-1);
+  test 0;
+  test 1;
+  test 2;
+  test 5;
+  print_endline "[Custom string]";
+  test_with_str "Toto" 1;
+  test_with_str "a" 5;
+  test_with_str "what" 3;
+  test_with_str "abc" 4;
+  test_with_str "" 4
