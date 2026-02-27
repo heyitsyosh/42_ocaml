1b84c61d2c4f8552d4fda55959fb2cd4d1eda73f
myoshika <47554536+heyitsyosh@users.noreply.github.com>
2026-02-27 12:28:24 +0100

    [refactor]: Merge tests into implementation files

--- module01/ex00/repeat_x.ml
+++ module01/ex00/repeat_x.ml
@@ -5,3 +5,16 @@ let rec repeat_x n =
     ""
   else
     "x" ^ repeat_x (n - 1)
+
+(* ────── Tests ────── *)
+
+let test n =
+  Printf.printf "%3d → %s\n" n (repeat_x n)
+
+let () =
+  print_endline "\x1b[4;37mrepeat_x:\x1b[0m";
+  test (-1);
+  test 0;
+  test 1;
+  test 2;
+  test 5
--- module01/ex01/repeat_string.ml
+++ module01/ex01/repeat_string.ml
@@ -5,3 +5,26 @@ let rec repeat_string ?str:(str="x") n =
     ""
   else
     str ^ repeat_string ~str (n - 1)
+
+(* ────── Tests ────── *)
+
+let test n =
+  Printf.printf "%3d → \"%s\"\n" n (repeat_string n)
+
+let test_with_str str n =
+  Printf.printf "%3d * \"%s\" → \"%s\"\n" n str (repeat_string ~str n)
+
+let () =
+  print_endline "\x1b[4;37mrepeat_string tests:\x1b[0m";
+  print_endline "[Default string]";
+  test (-1);
+  test 0;
+  test 1;
+  test 2;
+  test 5;
+  print_endline "[Custom string]";
+  test_with_str "Toto" 1;
+  test_with_str "a" 5;
+  test_with_str "what" 3;
+  test_with_str "abc" 4;
+  test_with_str "" 4
