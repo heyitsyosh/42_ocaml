9caf5d57e0af0551cf538407635a982abc0b0c3e
myoshika <47554536+heyitsyosh@users.noreply.github.com>
2026-02-27 10:42:05 +0100

    [refactor]: Merge tests into implementation files

--- module00/ex00/ft_test_sign.ml
+++ module00/ex00/ft_test_sign.ml
@@ -3,3 +3,15 @@ let ft_test_sign n =
 	  print_endline "positive"
   else
 	  print_endline "negative"
+
+(* ────── Tests ────── *)
+
+let test n =
+    Printf.printf "%d is " n;
+    ft_test_sign n
+
+let () =
+  print_endline "\x1b[4;37mft_test_sign:\x1b[0m";
+  test 42;
+  test 0;
+  test (-42)
