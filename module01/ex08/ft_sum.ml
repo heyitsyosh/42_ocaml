ec94985cccad0b32c392d59f92e438b529714b92
myoshika <47554536+heyitsyosh@users.noreply.github.com>
2026-03-02 15:58:21 +0100

    [feat]: Implement ex08, summation function

---
+++ module01/ex08/ft_sum.ml
@@ -0,0 +1,25 @@
+let ft_sum expr start_i last_i =
+  if last_i < start_i then
+    nan
+  else
+    let rec loop i sum =
+      if i = last_i then
+        sum +. expr i
+      else
+        loop (i + 1) (sum +. expr i)
+    in
+    loop start_i (float_of_int 0)
+
+(* ────── Tests ────── *)
+
+let test expr_str expr lower upper =
+  Printf.printf "%s [%d, %d] → " expr_str lower upper;
+  print_float(ft_sum expr lower upper);
+  print_newline ()
+
+let () =
+  print_endline "\x1b[4;37mft_sum:\x1b[0m";
+  test "i" (fun i -> float_of_int i) 3 1; 
+  test "i" (fun i -> float_of_int i) 1 3;
+  test "i * i"(fun i -> float_of_int (i * i)) 1 10;
+  test "1.0" (fun i -> 1.0) 1 5;
