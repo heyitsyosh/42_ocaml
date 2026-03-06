615189704a28352b6ffbffbbece0081474591708
myoshika <47554536+heyitsyosh@users.noreply.github.com>
2026-03-06 14:32:02 +0100

    [refactor]: Against subject.pdf, but naming suits as colors was icky

--- module03/ex00/main.ml
+++ module03/ex00/main.ml
@@ -1,9 +1,9 @@
 let () =
-  print_endline "\x1b[4;37mColor module:\x1b[0m";
+  print_endline "\x1b[4;37mSuit module:\x1b[0m";
   Printf.printf "%-7s %-4s\n" "Verbose" "Short";
   Printf.printf "%s\n" (String.make 12 '-');
   List.iter (fun c ->
     Printf.printf "%-7s %-4s\n"
-      (Color.toStringVerbose c)
-      (Color.toString c)
-  ) Color.all;
+      (Suit.toStringVerbose c)
+      (Suit.toString c)
+  ) Suit.all;
