fad1dfb1f49ad6fd83ff05218239ebee29b12243
myoshika <47554536+heyitsyosh@users.noreply.github.com>
2026-03-12 22:05:15 +0100

    [fix]: Generate grey code sequence using formula

--- module02/ex02/gray.ml
+++ module02/ex02/gray.ml
@@ -1,9 +1,9 @@
 let to_bits n width =
   let rec loop i acc =
-    if i < 0 then acc
-    else loop (i - 1) (((n lsr i) land 1) :: acc)
+    if i = width then acc
+    else loop (i + 1) (((n lsr i) land 1) :: acc)
   in
-  loop (width - 1) []
+  loop 0 []
 
 let rec print_bits = function
   | [] -> ()
@@ -11,37 +11,17 @@ let rec print_bits = function
     print_int bit;
     print_bits bits
 
-let prefix bit bits = bit :: bits
-
-let mirror lst =
-  let rec loop acc = function
-    | [] -> acc
-    | x :: xs -> loop (x :: acc) xs
-  in
-  loop [] lst
-
-let rec map f = function
-  | [] -> []
-  | x :: xs -> f x :: map f xs
-
 let gray n =
-  let rec build depth =
-    if depth = 1 then [[0]; [1]]
-    else
-      let prev = build (depth - 1) in
-      let first_half = map (prefix 0) prev in
-      let second_half = map (prefix 1) (mirror prev) in
-      first_half @ second_half
+  let total = 1 lsl n in
+  let rec loop i =
+    if i < total then (
+      let code = i lxor (i lsr 1) in
+      print_bits (to_bits code n);
+      if i < total - 1 then print_string ", ";
+      loop (i + 1)
+    )
   in
-  let sequence = build n in
-  let rec print_seq i = function
-    | [] -> ()
-    | bits :: rest ->
-      print_bits bits;
-      if rest <> [] then print_string ", ";
-      print_seq (i + 1) rest
-  in
-  print_seq 0 sequence;
+  loop 0;
   print_newline ()
 
 (* ────── Tests ────── *)
