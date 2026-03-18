423197792bc34e13a184b9dde13870b3a521abeb
myoshika <47554536+heyitsyosh@users.noreply.github.com>
2026-03-18 16:40:03 +0100

    [refactor]: Remove unused Failure

--- module04/ex00/micronap.ml
+++ module04/ex00/micronap.ml
@@ -3,7 +3,7 @@ let my_sleep () = Unix.sleep 1
 let () =
   if Array.length Sys.argv > 1 then
     let seconds = try int_of_string Sys.argv.(1)
-      with Failure _ -> exit 0
+      with _ -> exit 0
     in
     for i = 0 to seconds - 1 do
       print_string (string_of_int (i + 1) ^ "...");
