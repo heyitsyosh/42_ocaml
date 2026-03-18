ddcbcfb9d93b9097a77ce96d39ee8b7d4812baae
myoshika <47554536+heyitsyosh@users.noreply.github.com>
2026-03-18 16:22:49 +0100

    [feat]: Implement ex00, program to sleep for n seconds

---
+++ module04/ex00/micronap.ml
@@ -0,0 +1,13 @@
+let my_sleep () = Unix.sleep 1
+
+let () =
+  if Array.length Sys.argv > 1 then
+    let seconds = try int_of_string Sys.argv.(1)
+      with Failure _ -> exit 0
+    in
+    for i = 0 to seconds - 1 do
+      print_string (string_of_int (i + 1) ^ "...");
+      flush stdout;
+      my_sleep ()
+    done;
+    if seconds > 0 then print_newline ()
