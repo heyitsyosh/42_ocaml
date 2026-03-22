90fa887a75235c22ba3b2bd98f34378de397dcac
myoshika <47554536+heyitsyosh@users.noreply.github.com>
2026-03-22 19:52:34 +0100

    [refactor]: Edit test output

--- module05/ex00/ex00.ml
+++ module05/ex00/ex00.ml
@@ -1,6 +1,7 @@
 module StringSet = Set.Make(String)
 
 let () =
+  print_endline "\x1b[4;37mSet functor:\x1b[0m";
   let set = List.fold_right StringSet.add [ "foo"; "bar"; "baz"; "qux" ] StringSet.empty in
   StringSet.iter print_endline set;
   print_endline (StringSet.fold ( ^ ) set "")
